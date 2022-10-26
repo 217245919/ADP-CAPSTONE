ba5527cb9f431a718fe83a8883a8e8050300f7c0
217245919 <lylehaines.lh.lh@gmail.com>
2022-10-26 21:17:57 +0200

    updated work

--- Group19CarWash/Group19CarWash/src/main/java/za/ac/cput/Group19CarWash/services/CleanerService.java
+++ Group19CarWash/Group19CarWash/src/main/java/za/ac/cput/Group19CarWash/services/CleanerService.java
@@ -1,6 +1,7 @@
 package za.ac.cput.Group19CarWash.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import za.ac.cput.Group19CarWash.domain.Cleaner;
 import za.ac.cput.Group19CarWash.exception.CleanerNotFoundException;
 import za.ac.cput.Group19CarWash.repository.cleaner.ICleanerRepository;
@@ -8,6 +9,7 @@ import za.ac.cput.Group19CarWash.repository.cleaner.ICleanerRepository;
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class CleanerService {
 
     private static ICleanerRepository iCleanerRepository;
